1aa7fa10f9fcc4c61e0a507f9ce77b84ec7e1574
Tommy Hass <thassman@gmail.com>
2023-05-24 19:34:53 +0200

    added extra key that might contain the date information

--- main.py
+++ main.py
@@ -2,41 +2,46 @@ import os
 import sys
 from PIL import Image
 
-# Set list of valid file extensions
 VALID_EXTENSIONS = [".jpg", ".jpeg", ".png"]
 
 
-
-if len(sys.argv) < 1:
+if len(sys.argv) > 1:
     folder_path = sys.argv[1]
 else:
     folder_path = "photos"
 
 file_names = os.listdir(folder_path)
-
 for file_name in file_names:
 
     file_ext = os.path.splitext(file_name)[1]
     if (file_ext not in VALID_EXTENSIONS):
         continue
 
-    # Create the old file path
-    old_file_path = os.path.join(folder_path, file_name)
 
-    # Open the image
+    old_file_path = os.path.join(folder_path, file_name)
     image = Image.open(old_file_path)
 
-    # Get the date taken from EXIF metadata
-    date_taken = image._getexif()[36867]
+    if 36867 in image._getexif().keys():
+        date_taken = image._getexif()[36867]
+    elif 306 in image._getexif().keys():
+        date_taken = image._getexif()[306]
+    else:
+        print("No date in file: %s" % (file_name))
+        continue
+
     image.close()
 
-    # Reformat the date taken to "YYYYMMDD-HHmmss"
+    # Format date to "YYYYMMDD-HHmmss"
     date_time = date_taken \
         .replace(":", "")      \
         .replace(" ", "-")
 
     new_file_name = date_time + file_ext
     new_file_path = os.path.join(folder_path, new_file_name)
-    os.rename(old_file_path, new_file_path)
+    
+    if "tests" in new_file_path:
+        print("dryrun no renaming: ",new_file_name,new_file_path)
+    else:
+        os.rename(old_file_path, new_file_path)
 
 
